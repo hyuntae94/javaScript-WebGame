c2208e4098d04dcc9a3099776979e4a42d43cc2c
hyuntae <believe4279@gmail.com>
2022-01-05 18:24:28 +0900

    연산자버튼함수

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -5,32 +5,47 @@ let operator = "";
 const $operator = document.querySelector('#operator');
 const $result = document.querySelector('#result');
 
-//중복되는 부분
-const onClickNumber = (number) => {
-    return () => {
-        if (!operator){
-            numOne += number;
-        }
-        else{
-            numTwo += number;
-        }
-        $result.value += number;
+const onClickNumber = (event) => {
+    if (!operator){
+        numOne += event.target.textContent;
+        $result.value += event.target.textContent;
+        return ;
     }
+    //아래코드는 operator가 있을 경우에만 실행
+    if (!numTwo){
+        $result.value ="";
+    }
+    numTwo += event.target.textContent;
+    $result.value += event.target.textContent;
 }
 
-document.querySelector('#num-0').addEventListener('click', onClickNumber('0'));
-document.querySelector('#num-1').addEventListener('click', onClickNumber('1'));
-document.querySelector('#num-2').addEventListener('click', onClickNumber('2'));
-document.querySelector('#num-3').addEventListener('click', onClickNumber('3'));
-document.querySelector('#num-4').addEventListener('click', onClickNumber('4'));
-document.querySelector('#num-5').addEventListener('click', onClickNumber('5'));
-document.querySelector('#num-6').addEventListener('click', onClickNumber('6'));
-document.querySelector('#num-7').addEventListener('click', onClickNumber('7'));
-document.querySelector('#num-8').addEventListener('click', onClickNumber('8'));
-document.querySelector('#num-9').addEventListener('click', onClickNumber('9'));
-document.querySelector('#plus').addEventListener();
-document.querySelector('#minus').addEventListener();
-document.querySelector('#divide').addEventListener();
-document.querySelector('#mutiply').addEventListener();
+const onClickOperator = (op) => () => {
+    if (numOne){
+        operator = op;
+        $operator.value += op;
+    } 
+    else{
+        alert('숫자를 먼저 입력하세요!');
+    }
+    
+}
+
+//숫자(0~9)
+document.querySelector('#num-0').addEventListener('click', onClickNumber);
+document.querySelector('#num-1').addEventListener('click', onClickNumber);
+document.querySelector('#num-2').addEventListener('click', onClickNumber);
+document.querySelector('#num-3').addEventListener('click', onClickNumber);
+document.querySelector('#num-4').addEventListener('click', onClickNumber);
+document.querySelector('#num-5').addEventListener('click', onClickNumber);
+document.querySelector('#num-6').addEventListener('click', onClickNumber);
+document.querySelector('#num-7').addEventListener('click', onClickNumber);
+document.querySelector('#num-8').addEventListener('click', onClickNumber);
+document.querySelector('#num-9').addEventListener('click', onClickNumber);
+//연산자
+document.querySelector('#plus').addEventListener('click',onClickOperator('+'));
+document.querySelector('#minus').addEventListener('click',onClickOperator('-'));
+document.querySelector('#divide').addEventListener('click', onClickOperator('/'));
+document.querySelector('#multiply').addEventListener('click', onClickOperator('x'));
+//나머지
 document.querySelector('#clear').addEventListener();
 document.querySelector('#calculate').addEventListener();
