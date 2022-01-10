0f5da33266a3a34265ebc8056a0def5d58cccce4
hyuntae <believe4279@gmail.com>
2022-01-10 22:58:33 +0900

    feat: rselect random numer

--- numberBaseball/numberBaseball.js
+++ numberBaseball/numberBaseball.js
@@ -1,3 +1,13 @@
 const $form = document.querySelector('#form');
 const $input = document.querySelector('#input');
 const $logs = document.querySelector('#logs');
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+const answer = [];
+
+//무작위 숫자 4개 뽑기(중복X)
+for (let i=0; i<4; i++){
+    const index = Math.floor(Math.random()*numbers.length);
+    answer.push(numbers[index]);
+    numbers.splice(index,1);
+}
