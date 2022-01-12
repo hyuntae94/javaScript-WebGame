b979f967c14bb6a52fde942a85ca171055ae6869
hyuntae <believe4279@gmail.com>
2022-01-13 07:51:31 +0900

    add: create-rsp-game

---
+++ rsp/rsp.js
@@ -0,0 +1,6 @@
+const $computer = document.querySelector('#computer');
+const $scissors = document.querySelector('#scissors');
+const $rock = document.querySelector('#rock');
+const $paper = document.querySelector('#paper');
+const $score = document.querySelector('#score');
+
