bb7c4d531506f23932024d8728f6a90960899205
hyuntae <believe4279@gmail.com>
2022-01-12 15:50:48 +0900

    feat: drawball&&setTime

--- lottery/lottery.js
+++ lottery/lottery.js
@@ -10,3 +10,24 @@ while (candidate.length > 0){
     shuffle.push(candidate[randomIndex]);
     candidate.splice(randomIndex,1);
 }
+
+const winBalls = shuffle.slice(0,6).sort((a,b)=> a-b);
+const bonus = shuffle[6];
+
+const drawBall = (number, $parent) =>{
+    const $ball = document.createElement('div');
+    $ball.className = 'ball';
+    $ball.textContent = number;
+    $parent.appendChild($ball);
+}
+
+//6개의 숫자를 1초마다 공개한다.
+for (let i=0; i<winBalls.length; i++){
+    setTimeout(()=>{
+        drawBall(winBalls[i], $result)
+    }, 1000*(i+1));    
+}
+
+//마지막으로 보너스1개의 숫자를 공개한다.
+setTimeout(()=>{
+    drawBall(bonus, $bonus)},7000);
