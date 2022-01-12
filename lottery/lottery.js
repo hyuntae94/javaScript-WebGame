3af0f17aa5f0802c8526682ef51c8297b839fed5
hyuntae <believe4279@gmail.com>
2022-01-12 10:31:40 +0900

    feat: shuffle 45balls

--- lottery/lottery.js
+++ lottery/lottery.js
@@ -4,3 +4,9 @@ const $bonus = document.querySelector('#bonus');
 const candidate = Array(45).fill().map((_,i)=> i+1);
 const shuffle = [];
 
+//45개의 숫자 무작위로 섞기
+while (candidate.length > 0){
+    const randomIndex = Math.floor(Math.random()*candidate.length);
+    shuffle.push(candidate[randomIndex]);
+    candidate.splice(randomIndex,1);
+}
