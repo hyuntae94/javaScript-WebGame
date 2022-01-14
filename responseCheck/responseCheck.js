0ae287cb558842ec23e461d557ec58c84660fe80
hyuntae <believe4279@gmail.com>
2022-01-14 09:53:38 +0900

    add: responseCheck folder

---
+++ responseCheck/responseCheck.js
@@ -0,0 +1,15 @@
+const $screen = document.querySelector('#screen');
+const $result = document.querySelector('#result');
+
+$screen.addEventListener('click', ()=>{
+    if ($screen.classList.contains('waiting')){
+        //대기화면
+    }
+    else if ($screen.classList.contains('ready')){
+        //준비화면
+    }
+    else if ($screen.classList.contains('now')){
+        //클릭화면
+    }
+
+})
