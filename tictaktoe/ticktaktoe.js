23a7bf238a9bead8af2c977c203ef0e744ac8bb2
hyuntae <believe4279@gmail.com>
2022-01-17 14:07:54 +0900

    feat: checkWinnerHandle && drawCase

--- tictaktoe/ticktaktoe.js
+++ tictaktoe/ticktaktoe.js
@@ -5,14 +5,69 @@ const rows = [];
 
 let turn = 'O';
 
+const checkWinnerHandler = (target) => {
+    let rowIndex = target.parentNode.rowIndex;
+        cellIndex = target.cellIndex;
+    let hasWinner = false; // 승자여부판별 변수
+    
+    //가로줄 검사
+    if (
+        rows[rowIndex][0].textContent === turn &&
+        rows[rowIndex][1].textContent === turn &&
+        rows[rowIndex][2].textContent === turn)
+        {
+            hasWinner = true;
+        }
+    //세로줄 검사
+    if (
+        rows[0][cellIndex].textContent === turn &&
+        rows[1][cellIndex].textContent === turn &&
+        rows[2][cellIndex].textContent === turn)
+        {
+            hasWinner = true;
+        }
+    //대각선 검사
+    if (
+        rows[0][0].textContent === turn &&
+        rows[1][1].textContent === turn &&
+        rows[2][2].textContent === turn 
+    ) {
+        hasWinner = true;
+    }
+    if (
+        rows[0][2].textContent === turn &&
+        rows[1][1].textContent === turn &&
+        rows[2][0].textContent === turn
+    ) {
+        hasWinner = true;
+    }
+    return (hasWinner);
+}
+
+
 const tableClickHandler = (event) =>{
-    if (event.target.textContent !== ""){
-        console.log('빈칸이 아닙니다');
+    if (event.target.textContent !== ""){//칸이 채워져 있다면
+        alert('빈칸이 아닙니다!!');
+        return ;
     }
-    else {
-        event.target.textContent = turn;
-        turn = turn === 'O' ? 'X' : 'O';
+    //빈칸이라면
+    event.target.textContent = turn;    
+
+    const hasWinner = checkWinnerHandler(event.target);
+    if (hasWinner){//승자가 있다면
+        $result.textContent = `${turn}님이 승리!`;
+        $table.removeEventListener('click',tableClickHandler);
+        return ;
     }
+    //승자가 없다면
+    //무승부인지 확인
+    const draw = rows.flat().every((cell)=>cell.textContent);
+    if (draw){
+        $result.textContent = '무승부';
+        return ;
+    }
+    //승자도 없고 무승부가 아니라면 현재 게임진행중
+    turn = turn === 'O' ? 'X' : 'O';
 }
 
 
