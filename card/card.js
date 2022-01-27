35bd43d01e77a2a32dd4e603b92270a7e42e0e71
hyuntae <believe4279@gmail.com>
2022-01-27 23:11:08 +0900

    feat: playTime && totalCard Select

--- card/card.js
+++ card/card.js
@@ -1,14 +1,22 @@
 const $wrapper = document.querySelector('#wrapper');
 
-const total = 12;
-const colors = ['red','orange','yellow','green','white','pink'];
-let colorCopy = colors.concat(colors);
+const colors = ['red','orange','yellow','green','white','pink','purple','lime','aqua','blueviolet'];
+const totalCard = Number(prompt('생성 카드 수를 입력하세요. 최대 카드수는 20장입니다.'))
+const total = totalCard;
+let colorCopy = [];
 let shuffled = [];
 let clicked = [];
 let completed = [];
 let clickable = false;
+let startDate;
+
 
 const shuffle = () => {//무작위 카드 섞기
+    for (let i = 0; i < total/2; i++){//10종류중 x장만큼 랜덤 뽑기
+        const randomIndex = Math.floor(Math.random()*colors.length);
+        colorCopy = colorCopy.concat(colors.splice(randomIndex,1));
+    }
+    colorCopy = colorCopy.concat(colorCopy);
     while(colorCopy.length > 0){
         const randomIndex = Math.floor(Math.random()*colorCopy.length);
         shuffled = shuffled.concat(colorCopy.splice(randomIndex,1));
@@ -49,8 +57,10 @@ function onClickCard(){
         if (completed.length !== total){
             return ;
         }
+        //카드를 다 맞춘경우
+        const endDate = new Date();
         setTimeout(()=>{
-            alert('축하합니다!');
+            alert(`축하합니다! ${parseInt((endDate-startDate)/1000)}초 걸렸습니다`);
             resetGame();
         },1000);
         return ;
@@ -67,6 +77,10 @@ function onClickCard(){
 }
 
 const startGame = () => {
+    if (totalCard % 2 !==0 || totalCard > 20) {
+        alert('짝수값이여야 합니다.')
+        return ;
+    }
     shuffle();
     for (let i = 0; i < total; i++){
         const card = createCard(i);
@@ -85,6 +99,7 @@ const startGame = () => {
             card.classList.remove('flipped');
         })
         clickable = true;
+        startDate = new Date();
     },5000)
 }
 
