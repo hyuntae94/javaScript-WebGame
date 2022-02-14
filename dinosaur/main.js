ed8c724aa84a2101755d689c9e2b07cc61d982f5
hyuntae <believe4279@gmail.com>
2022-02-14 16:48:27 +0900

    feat: dinosaur jump&down

--- dinosaur/main.js
+++ dinosaur/main.js
@@ -31,22 +31,48 @@ class Cactus{
 
 let timer = 0;
 let cactusArr = [];
+let 점프중 = false;
+let 점프timer = 0;
 
 function 프레임마다실행할거(){
     requestAnimationFrame(프레임마다실행할거);
     timer++;
     ctx.clearRect(0,0,canvas.width,canvas.height);
     
-    if (timer % 120 == 0){
+    if (timer % 90   == 0){
         let cactus = new Cactus();
         cactusArr.push(cactus);
     }
 
-    cactusArr.forEach(a => {
-        a.x--;
+    cactusArr.forEach((a,i) => {
+        if (a.x < 0){
+            cactusArr.splice(i,1);
+        }
+        a.x -= 3;
         a.draw();
     });
+
+    // //스페이스바 누르면 점프
+    if (점프중 === true){
+        dino.y -= 3;
+        점프timer += 3;
+    }
+    if (점프중 === false){
+        if (dino.y < 200){
+            dino.y += 2.4;
+        }
+    }
+    if (점프timer > 100){
+        점프중 = false;
+        점프timer = 0;
+    }
 
     dino.draw();
 }
 프레임마다실행할거();
+
+document.addEventListener('keydown', function(e){
+    if (e.code === 'Space'){
+        점프중 = true;
+    }
+})
