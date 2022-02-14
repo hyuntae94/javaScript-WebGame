9863fa347cab5beebadcb6d56e54e1b725c71234
hyuntae <believe4279@gmail.com>
2022-02-14 17:32:14 +0900

    feat: dino&cactus collision detection

--- dinosaur/main.js
+++ dinosaur/main.js
@@ -4,6 +4,8 @@ let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
 
+let dinoImg = new Image();
+dinoImg.src = 'dinosaur.png';
 
 let dino = {
     x : 10,
@@ -12,10 +14,13 @@ let dino = {
     height : 50,
     draw(){
         ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(dinoImg, this.x, this.y);
     }
 }
 
+let cactusImg = new Image();
+cactusImg.src = 'cactus.png';
 class Cactus{
     constructor(){
         this.x = 500;
@@ -25,7 +30,8 @@ class Cactus{
     }
     draw(){
         ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(cactusImg,this.x,this.y);
     }
 }
 
@@ -33,9 +39,10 @@ let timer = 0;
 let cactusArr = [];
 let 점프중 = false;
 let 점프timer = 0;
+let animation;
 
 function 프레임마다실행할거(){
-    requestAnimationFrame(프레임마다실행할거);
+    animation = requestAnimationFrame(프레임마다실행할거);
     timer++;
     ctx.clearRect(0,0,canvas.width,canvas.height);
     
@@ -50,12 +57,14 @@ function 프레임마다실행할거(){
         }
         a.x -= 3;
         a.draw();
+
+        충돌하냐(dino,a);
     });
 
     // //스페이스바 누르면 점프
     if (점프중 === true){
-        dino.y -= 3;
-        점프timer += 3;
+        dino.y -= 4;
+        점프timer += 4;
     }
     if (점프중 === false){
         if (dino.y < 200){
@@ -70,6 +79,16 @@ function 프레임마다실행할거(){
     dino.draw();
 }
 프레임마다실행할거();
+
+//충돌확인
+function 충돌하냐(dino, cactus){
+    let x축차이 = cactus.x - (dino.x + dino.width);
+    let y축차이 = cactus.y - (dino.y + dino.height);
+    if (x축차이 < 0 && y축차이 < 0){
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        cancelAnimationFrame(animation);
+    }
+}
 
 document.addEventListener('keydown', function(e){
     if (e.code === 'Space'){
